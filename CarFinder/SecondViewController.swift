5092043794e456470a68666bd7a773a4706465ea
Steve Cho <iamstevecho@gmail.com>
2016-04-18 22:11:47 -0400

    Initial Commit

---
+++ CarFinder/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CarFinder
+//
+//  Created by Steve Cho on 2016-04-18.
+//  Copyright © 2016 Steve Cho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
